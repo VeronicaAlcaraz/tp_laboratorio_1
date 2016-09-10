d1435be014f55e71d0a40a3c9e7fbb063c34cf75
VeronicaAlcaraz <pj.random@hotmail.com>
2016-09-09 21:35:31 -0300

    Update

--- TP1_Alcaraz/funciones.c
+++ TP1_Alcaraz/funciones.c
@@ -3,16 +3,41 @@
 #include <conio.h>
 #include "funciones.h"
 
+/** \brief funcion que suma dos numeros ingresados.
+ * \param primer numero ingresado.
+ * \param segundo numero ingresado.
+ * \return resultado de la suma.
+ */
+ 
+ 
 void suma(float x, float y)
 {
     printf("La suma de A+B es: %.2f\n", x+y);
 }
 
+
+
+/** \brief funcion que resta dos numeros ingresados.
+ * \param primer numero ingresado.
+ * \param segundo numero ingresado.
+ * \return resultado de la resta.
+ */
+ 
+ 
 void resta(float x, float y)
 {
     printf("La resta entre A-B es: %.2f\n", (x-y));
 }
 
+
+
+/** \brief funcion que divide dos numeros ingresados.
+ * \param primer numero ingresado.
+ * \param segundo numero ingresado.
+ * \return resultado de la division.
+ */
+ 
+ 
 void division(float x, float y)
 {
     if(y==0)
@@ -21,11 +46,29 @@ void division(float x, float y)
         printf("La division es: %.2f\n", x/y);
 }
 
+
+
+/** \brief funcion que multiplica dos numeros ingresados.
+ * \param primer numero ingresado.
+ * \param segundo numero ingresado.
+ * \return resultado de la multiplicacion.
+ */
+ 
+ 
 void multiplicar(float x, float y)
 {
     printf("La multiplicacion de A*B es %.2f\n", x*y);
 }
 
+
+
+/** \brief funcion que da el factorial de un numero.
+ * \param primer numero ingresado.
+ * \param segundo numero ingresado.
+ * \return resultado del factorial.
+ */
+ 
+ 
 void factorial(float x)
 {
     float fac=1;
@@ -43,6 +86,16 @@ void factorial(float x)
    }
 }
 
+
+
+
+/** \brief imprime un mensaje por pantalla y lee un caracter del teclado para continuar.
+ * \param 
+ * \param 
+ * \return tecla seleccionada.
+ */
+ 
+ 
 void mensaje()
 {
     printf("Presione cualquier tecla para continuar");
